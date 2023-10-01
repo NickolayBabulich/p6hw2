fe24251c15c3f23095bce4d3b177f97b0dc5f3c3
Nickolas Green <nickolas@Nickolass-MacBook-Pro.local>
2023-10-01 13:19:40 +0700

    add, update, fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import index, contacts, product
+
+app_name = CatalogConfig.name
+
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts)
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>', product, name='product')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from catalog.models import Product
 
+
 # Create your views here.
 def index(request):
-    print(f'{Product.objects.all()}')
-    return render(request, 'catalog/index.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Каталог продуктов'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
@@ -13,5 +18,16 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name} {phone} {message}')
+    context = {
+        'title': ' Контактная информация',
+    }
 
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk)
+    }
+
+    return render(request, 'catalog/product.html', context)
